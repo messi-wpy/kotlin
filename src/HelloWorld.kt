e63e6ee603f34b5ffa104332665db6a17cc29f51
messi-wpy <904315105@qq.com>
2019-08-01 17:10:34 +0800

    kotlin 初试

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,6 +1,10 @@
+import java.beans.Expression
+import java.lang.IllegalArgumentException
+import java.util.*
+
 fun main(args:Array<String>){
     println("hello, world!")
-    hello(Array(2) {"wpy,messi"})
+    loopTest()
 }
 
 fun hello(args: Array<String>){
@@ -24,4 +28,69 @@ var answers="i am student"
 
 //kotlin中建立这种类似c中struct的值对象很方便把
 class Person(val name: String,val age:Int)
+class Rectangle(val height:Int,val width:Int){
+    val isSquare:Boolean
+    get() = height==width
 
+}
+fun classtest(){
+    val person=Person("Bob",20)
+    println(person.age)
+    val rec=Rectangle(20,20)
+    println(rec.isSquare)
+    val t=test()
+    println(t.x)
+
+}
+class test{
+    var x:Int = 0
+}
+enum class Color{
+    RED,YELLOW,BLUE;
+
+    fun getWarmth(color: Color)=when(color){
+        RED->"warm"
+        YELLOW,BLUE->"cold"
+
+    }
+}
+enum class ColorRgb(val r:Int,val g:Int,val b:Int){
+    RED(255,0,0),YELLOW(255,255,0),BLUE(0,0,255);
+
+    fun rgb()=(r*256+g)*256+b
+}
+
+
+interface Expr
+class Num(val value:Int):Expr
+class Sum(val left:Expr,val right:Expr):Expr
+
+fun eval(e:Expr):Int=
+    when(e){
+    is Num->
+        e.value
+    is Sum->
+        eval(e.right)+eval(e.left)
+    else->
+        throw IllegalArgumentException("Unknown expression")
+
+}
+
+fun loopTest(){
+    //  for (i in 100 downTo 1 step 2)
+    //     println(i)
+    for (i in 1..100 )
+        println(i)
+}
+
+//迭代map
+val binaryReps=TreeMap<Char,String>()
+fun iterateMap(map:TreeMap<Char,String>){
+    for (c in 'a'..'f'){
+        val binary=Integer.toBinaryString(c.toInt())
+        binaryReps[c]=binary
+    }
+    for ((letter,binary) in binaryReps){
+        println("$letter = $binary")
+    }
+}
