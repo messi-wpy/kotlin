b8f7aef1907073159a2f733acb510fdaa4bebce0
messi-wpy <904315105@qq.com>
2019-10-24 21:35:11 +0800

    lambda

--- src/Lambda.kt
+++ src/Lambda.kt
@@ -1,5 +1,5 @@
 import java.io.File
-import java.lang.StringBuilder
+import kotlin.text.StringBuilder
 
 fun flatMapTest() {
     val strings = listOf("abc", "def")
@@ -69,7 +69,11 @@ fun alphabet3()=StringBuilder().apply {
 
 fun main(){
 
-    print(alphabet())
+    //print(alphabet())
+
+    test2()
+    val a= listOf("asd","ASd","ASd","Ddd")
+    println(a.joinString(){it[2].toString()})
 
 }
 
@@ -86,5 +90,66 @@ class Student(val school:String,val name: String){
     }
 }
 
+fun test2(){
+    var s="asdsadaaaadsda"
+    s=s.filter { it=='a' }
+    println(s)
+}
+
+
+val sum={a:Int,y:Int->a+y}
+
+fun String.filter(pred:(Char)->Boolean):String{
+    val sb=StringBuilder()
+    println("my fun")
+    for (index in 0 until length){
+        val element=get(index)
+        if (pred(element))
+            sb.append(element)
+    }
+    return sb.toString()
+
+}
+
+//kotlin 语法约定：如果lambda表达式时函数调用的最后一个实参，可以放到括号外边，
+//如果是唯一实参，可以去掉括号
+fun <T>Collection<T>.joinString(
+    separator:String=",",
+    prefix:String="",
+    postfix:String="",
+    transform:((T)->String)?=null
+
+):String{
+    val  result=StringBuilder(prefix)
+
+    for ((index,element) in this.withIndex()){
+        if (index>0)result.append(separator)
+        result.append(transform?.invoke(element)?:element.toString())
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+//返回函数的函数
+
+enum class Delivery{SSSS,TTTT}
+class Order(val itemCount:Int)
+
+fun getMethod(d:Delivery):(Order)->Double{
+    if (d==Delivery.SSSS)
+        return { order ->
+            print("test")
+            0.11
+        }
+
+    return { 1.2*it.itemCount }
+
+}
+
+fun  testReturn(){
+    val calcutor=getMethod(Delivery.SSSS)
+    println(calcutor(Order(999)))
+
+}
 
 
